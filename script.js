21e70b798d619335ccecfcecec1633adc4141b7f
Imran Hossain Sagor <imran@sagor.Imran>
2020-08-08 01:41:38 +0600

    Password Generator

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+function loader() {
+    var charac = [];
+    var container = '';
+
+    for (let i = 36; i<= 126; i++) {
+        if (i == 44 || i == 124 || i == 38 || i == 39 || i == 43 || i == 45 || i == 58 || i == 59 || i == 60 || i == 61 || i == 62 || i == 39) {
+            continue;
+        }
+        charac.push(i);
+    }
+
+    for (let i = 1; i<= 10; i++) {
+        container += String.fromCharCode(charac[Math.floor(Math.random() * charac.length )]);
+    }
+
+    document.getElementById('pass').value = container;
+}
+
+function copier() {
+    pass.select();
+    document.execCommand('copy');
+}
